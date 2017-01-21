70c3b56536f6a6c06212ea726b9bd1dc3f490942
student <student>
2017-01-21 15:13:57 +0900

    edit event.json and class

--- Assets/Scripts/Eevnt/EventManager.cs
+++ Assets/Scripts/Eevnt/EventManager.cs
@@ -6,15 +6,25 @@ using System.Collections;
 using System.Collections.Generic;
 
 [Serializable]
-public class EventQuest
+public class EventJson
 {
-
+    public int e_id;
+    public string e_name;
+    public string target_type;
+    public string target_name;
+    public int target_num;
+    public string prefab_name;
+    public float target_pos_X;
+    public float target_pos_Y;
+    public float target_pos_Z;
+    public int e_present;
+    public bool is_finish;
 }
 
 [Serializable]
 public class EventCollect
 {
-    public EventQuest[] events;
+    public EventJson[] events;
 }
 
 public class EventManager : SingletonMonoBehaviour<EventManager> {
@@ -28,4 +38,10 @@ public class EventManager : SingletonMonoBehaviour<EventManager> {
 	void Update () {
 	
 	}
+    public EventJson ReadFile(string fileName)
+    {
+        EventJson result;
+        //
+        return result;
+    }
 }
