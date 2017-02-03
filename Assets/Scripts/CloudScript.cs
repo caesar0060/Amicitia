878794da6ed0174af213e895c8fdeeb19f02da41
student <student>
2017-02-03 19:19:26 +0900

    YEAH　焼肉

---
+++ Assets/Scripts/CloudScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudScript : MonoBehaviour {
+    float num = 0;
+    float time = 0;
+	// Use this for initialization
+	void Start () {
+        time = Time.time;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, num, 0));
+        num = (Time.time - time) * 2;
+        if (num > 360)
+            num = 0;
+    }
+}
---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dragon : MonoBehaviour {
+    float num = 0;
+    float time = 0;
+    // Use this for initialization
+    void Start()
+    {
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 old_pos = this.transform.position;
+        float x = Mathf.Rad2Deg * Mathf.Sin(num);
+        float z = Mathf.Rad2Deg * Mathf.Cos(num);;
+        this.transform.position = new Vector3(x, 20, z);
+        Quaternion rotationPos = Quaternion.LookRotation(this.transform.position - old_pos);
+        this.transform.rotation = rotationPos;
+        num = (Time.time - time);
+        if (num > 360)
+            num = 0;
+    }
+}
